8cdc7d03f207b1c76ce5446d44baffdf47d9ca81
ErlassErlass <erlasserlass.official02@gmail.com>
2026-08-11 02:24:05 +0000

    fix(auth): add Indonesian translation file for authentication errors and fix email typo for instructor Yusup [v2.9.1]

---
+++ lang/id/auth.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Baris-baris Bahasa Otentikasi (Authentication Language Lines)
+    |--------------------------------------------------------------------------
+    */
+
+    'failed' => 'Kombinasi Email / ID Instruktur dan Kata Sandi yang Anda masukkan tidak cocok.',
+    'password' => 'Kata sandi yang Anda masukkan salah.',
+    'throttle' => 'Terlalu banyak percobaan login. Silakan coba lagi dalam :seconds detik.',
+
+];
